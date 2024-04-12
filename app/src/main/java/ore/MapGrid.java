25fdfeb953b109dd8e4ad0ba2e790ec51974668f
jlinehan-01 <jlinehan@student.unimelb.edu.au>
2024-04-12 14:24:26 +1000

    remove HashMap from mapgrid

--- app/src/main/java/ore/MapGrid.java
+++ app/src/main/java/ore/MapGrid.java
@@ -1,10 +1,5 @@
 package ore;
 
-import ch.aplu.jgamegrid.Location;
-import ch.aplu.jgamegrid.Actor;
-import java.util.ArrayList;
-import java.util.HashMap;
-
 /**
  * The MapGrid class represents a grid-based map for an ore simulation game.
  * It stores information about the elements present in each cell of the grid,
@@ -42,6 +37,12 @@ public class MapGrid {
             "x..........x", // 7
             "xxxxxxxxxxxx"};// 8
 
+    public String[] getMap() {
+        return map;
+    }
+
+    private final String[] map;
+
     public int getNumHorzCells() {
         return numHorzCells;
     }
@@ -49,13 +50,9 @@ public class MapGrid {
     public int getNumVertCells() {
         return numVertCells;
     }
-    public ArrayList<Actor> get(Location location) {
-        return map.get(location);
-    }
 
     private final int numHorzCells;
     private final int numVertCells;
-    private final HashMap<Location, ArrayList<Actor>> map = new HashMap<>();
 
     /**
      * Mapping from the string to a HashMap to prepare drawing
@@ -63,99 +60,16 @@ public class MapGrid {
      * @param model An enum specifying the map to generate
      */
     public MapGrid(int model) {
-        String[] map_str;
+        // set map
         if (model == 0) {
             // model == 0
-            numHorzCells = map_0[0].length();
-            numVertCells = map_0.length;
-            map_str = map_0;
+            map = map_0;
         } else {
             // model == 1
-            numHorzCells = map_1[0].length();
-            numVertCells = map_1.length;
-            map_str = map_1;
+            map = map_1;
         }
-
-        // Copy structure into HashMap
-        Location location;
-        for (int y = 0; y < numVertCells; y++) {
-            for (int x = 0; x < numHorzCells; x++) {
-                location = new Location(x, y);
-                map.put(location, new ArrayList<Actor>());
-                switch (map_str[y].charAt(x)) {
-                    case '.': // Empty
-                        map.get(location).add(new Floor());
-                        break;
-                    case 'x': // Border
-                        map.get(location).add(new Border());
-                        break;
-                    case '*': // Ore
-                        map.get(location).add(new Ore());
-                        break;
-                    case 'o': // Target
-                        map.get(location).add(new Target());
-                        break;
-                    case 'P': // Pusher
-                        map.get(location).add(new Pusher());
-                        break;
-                    case 'B': // Bulldozer
-                        map.get(location).add(new Bulldozer());
-                        break;
-                    case 'E': // Excavator
-                        map.get(location).add(new Excavator());
-                        break;
-                    case 'R': // Rock
-                        map.get(location).add(new Rock());
-                        break;
-                    case 'D': // Clay
-                        map.get(location).add(new Clay());
-                        break;
-                }
-            }
-        }
-    }
-
-    /**
-     * gets the number of ores that have been moved to targets by checking for an ore and a target in each location
-     * @return the number of ores in a target location
-     */
-    public int getOresDone()
-    {
-        int oresDone = 0;
-        for (Location location: map.keySet()) {
-            if (contains(location, OreSim.ElementType.ORE) && contains(location, OreSim.ElementType.TARGET))
-            {
-                oresDone++;
-            }
-        }
-        return oresDone;
-    }
-
-    public boolean completed()
-    {
-        // count number of ores
-        int numOres = 0;
-        for (Location location: map.keySet()) {
-            for (MapObject mapObject: map.get(location))
-            {
-                if (mapObject.getType() == OreSim.ElementType.ORE)
-                {
-                    numOres++;
-                }
-            }
-        }
-        return numOres == getOresDone();
-    }
-
-    public boolean contains(Location location, OreSim.ElementType type)
-    {
-        for (MapObject mapObject: map.get(location))
-        {
-            if (mapObject.getType() == type)
-            {
-                return true;
-            }
-        }
-        return false;
+        // set vertical and horizontal number of cells
+        numHorzCells = map[0].length();
+        numVertCells = map.length;
     }
 }
